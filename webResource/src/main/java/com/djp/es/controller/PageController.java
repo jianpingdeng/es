dd73afeb3659ffd0069407bb8da5fc2003190c78
jianpingdeng <djp5188@yeah.net>
2015-08-11 16:07:40 +0800

    init option

---
+++ webResource/src/main/java/com/djp/es/controller/PageController.java
@@ -0,0 +1,19 @@
+package com.djp.es.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by dengjianping on 2015/8/11.
+ */
+@Controller
+public class PageController {
+    Logger log = LoggerFactory.getLogger(PageController.class);
+    @RequestMapping("/pages/index")
+    public String index(){
+        log.info("go to index.jsp");
+        return "/index";
+    }
+}
